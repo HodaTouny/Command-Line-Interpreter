b3738fca7d290d40daaec3a58d078db648d54f47
mayahesham <96448594+mayahesham@users.noreply.github.com>
2023-10-28 23:04:01 +0200

    add parser functions

--- src/Parser.java
+++ src/Parser.java
@@ -1,15 +1,22 @@
-
 public class Parser {
-    String commandName;
-    String[] args;
-    public boolean parse(String input){
+    private String commandName;
+    private String[] args;
+    public boolean parse(String input) {
+        String[] tokens = input.trim().split("\\s+");
+        if (tokens.length > 0) {
+            commandName = tokens[0];
+            args = new String[tokens.length - 1];
+            System.arraycopy(tokens, 1, args, 0, tokens.length - 1);
+            return true;
+        }
         return false;
     }
-    public String getCommandName(){
-        return null;
-    }
-    public String[] getArgs(){
-        return new String[0];
+    public String getCommandName() {
+        return commandName;
     }
 
+    public String[] getArgs() {
+        return args;
+    }
 }
+
