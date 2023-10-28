5e58b9b250f2a5df11fd923a93baa7b8ab36ddc7
Hoda Touny <100847847+HodaTouny@users.noreply.github.com>
2023-10-28 19:12:04 +0200

    initial

---
+++ src/Parser.java
@@ -0,0 +1,15 @@
+
+public class Parser {
+    String commandName;
+    String[] args;
+    public boolean parse(String input){
+        return false;
+    }
+    public String getCommandName(){
+        return null;
+    }
+    public String[] getArgs(){
+        return new String[0];
+    }
+
+}
---
+++ src/Terminal.java
@@ -0,0 +1,96 @@
+import java.util.List;
+
+public class Terminal {
+
+    Parser parser;
+
+    public String pwd() {
+
+        return null;
+    }
+
+    public void cd(String[] args) {
+
+    }
+
+    public void ls() {
+    }
+
+    public void lsReverse() {
+
+    }
+
+    public void mkdir(String[] args) {
+
+    }
+
+    public void rmdir(String argument) {
+
+    }
+
+    public void touch(String argument) {
+
+    }
+
+    public void cp(String[] args) {
+
+    }
+
+    public void cpRecursive(String[] args) {
+
+    }
+
+    public void rm(String filename) {
+
+    }
+
+    public void cat(String[] args) {
+
+    }
+
+    public void wc(String filename) {
+
+    }
+
+    public void redirectOutput(String command, String filename) {
+
+    }
+
+    public void appendOutput(String command, String filename) {
+
+    }
+
+    public void history() {
+
+    }
+
+    public void exit() {
+
+    }
+    private static boolean isValidPath(String path) {
+        return false;
+    }
+
+    private static boolean isFile(String path) {
+        return false;
+    }
+
+    private static boolean isDirectory(String path) {
+        return false;
+    }
+
+    private static List<String> listFilesInDirectory(String path) {
+        return null;
+    }
+
+
+    public void chooseCommandAction(){
+
+    }
+
+    
+    public static void main(String[] args){
+
+    }
+
+}
