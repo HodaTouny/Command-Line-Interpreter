436434c835dcfe9b720e967d656babea69a69c7a
mayahesham <96448594+mayahesham@users.noreply.github.com>
2023-10-28 23:05:26 +0200

    add main menu+echo+pwd+ls+ls -r

--- src/Terminal.java
+++ src/Terminal.java
@@ -1,96 +1,102 @@
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Terminal {
+    private Path currentDirectory;
 
-    Parser parser;
+    public Terminal() {
+        currentDirectory = Paths.get(System.getProperty("user.dir"));
+    }
 
     public String pwd() {
-
-        return null;
+        return currentDirectory.toString();
     }
 
-    public void cd(String[] args) {
-
+    public void echo(String args[]) {
+        for (String arg : args) {
+            System.out.print(arg + " ");
+        }
+        System.out.println();
     }
 
-    public void ls() {
+    public void getFiles() {
+        File directory = currentDirectory.toFile();
+        File[] directoryContents = directory.listFiles();
+        Arrays.sort(directoryContents);
+        for (File content : directoryContents) {
+            if (content.isFile() || content.isDirectory()) {
+                System.out.println(content.getName());
+            }
+        }
     }
 
-    public void lsReverse() {
+    public void Lsr() {
+        File directory = currentDirectory.toFile();
+        File[] directoryContents = directory.listFiles();
+        Arrays.sort(directoryContents);
 
+        List<File> itemList = Arrays.asList(directoryContents);
+        Collections.reverse(itemList);
+        for (File content : itemList) {
+            if (content.isFile() || content.isDirectory()) {
+                System.out.println(content.getName());
+            }
+        }
     }
 
-    public void mkdir(String[] args) {
-
+    public void mKdir(String[] args) {
+        for (String dirName : args) {
+            Path newDir = currentDirectory.resolve(dirName);
+            File newDirFile = newDir.toFile();
+            if (!newDirFile.exists()) {
+                if (newDirFile.mkdir()) {
+                    System.out.println("Directory '" + dirName + "' created successfully.");
+                } else {
+                    System.out.println("Failed to create directory '" + dirName + "'.");
+                }
+            } else {
+                System.out.println("Directory '" + dirName + "' already exists.");
+            }
+        }
+    }
+    public void chooseCommandAction(String commandName, String[] commandArgs) {
+        if (commandName.equals("echo")) {
+            echo(commandArgs);
+         } else if (commandName.equals("pwd")) {
+        System.out.println(pwd());
+       }
+        else if (commandName.equals("ls")) {
+            getFiles();
+        }
+        else if (commandName.equals("ls -r")) {
+            Lsr();
+        }
+        else if (commandName.equals("mkdir")) {
+           mKdir(commandArgs);
+        }else if (commandName.equals("exit")) {
+            System.exit(0);
+        } else {
+            System.out.println("Command not recognized");
+        }
     }
 
-    public void rmdir(String argument) {
-
+    public static void main(String[] args) {
+        Terminal terminal = new Terminal();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print(terminal.pwd() + ">");
+            String input = scanner.nextLine();
+            Parser parser = new Parser();
+            if (parser.parse(input)) {
+                String commandName = parser.getCommandName();
+                String[] commandArgs = parser.getArgs();
+                terminal.chooseCommandAction(commandName, commandArgs);
+            }
+        }
     }
-
-    public void touch(String argument) {
-
-    }
-
-    public void cp(String[] args) {
-
-    }
-
-    public void cpRecursive(String[] args) {
-
-    }
-
-    public void rm(String filename) {
-
-    }
-
-    public void cat(String[] args) {
-
-    }
-
-    public void wc(String filename) {
-
-    }
-
-    public void redirectOutput(String command, String filename) {
-
-    }
-
-    public void appendOutput(String command, String filename) {
-
-    }
-
-    public void history() {
-
-    }
-
-    public void exit() {
-
-    }
-    private static boolean isValidPath(String path) {
-        return false;
-    }
-
-    private static boolean isFile(String path) {
-        return false;
-    }
-
-    private static boolean isDirectory(String path) {
-        return false;
-    }
-
-    private static List<String> listFilesInDirectory(String path) {
-        return null;
-    }
-
-
-    public void chooseCommandAction(){
-
-    }
-
-    
-    public static void main(String[] args){
-
-    }
-
 }
